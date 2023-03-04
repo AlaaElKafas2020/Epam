dfb2b0075e83602f7f249bf7b43c3234502e2211
Alaa ElKaffass <icalaa.elkaffass@americanwell.com>
2023-03-04 22:29:32 +0100

    will add task for spring testing module here

--- ticket-booking/src/main/java/com/springboot/first/app/BookingRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.first.app;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-}
